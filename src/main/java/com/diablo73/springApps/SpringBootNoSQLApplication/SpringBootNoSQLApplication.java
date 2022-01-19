5aadd1dde596b719cd9f74a68b00b879eb910477
Diablo73 <ashul2173@gmail.com>
2022-01-19 18:43:01 +0530

    Successfully Started SpringBoot Application

---
+++ src/main/java/com/diablo73/springApps/SpringBootNoSQLApplication/SpringBootNoSQLApplication.java
@@ -0,0 +1,13 @@
+package com.diablo73.springApps.SpringBootNoSQLApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNoSQLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNoSQLApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diablo73/springApps/SpringBootNoSQLApplication/SpringBootNoSQLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diablo73.springApps.SpringBootNoSQLApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNoSQLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
