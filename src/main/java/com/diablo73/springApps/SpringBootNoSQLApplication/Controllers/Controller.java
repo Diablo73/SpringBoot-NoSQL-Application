312cfedb8e45d0da95adabc9c29cefbf4c202104
Diablo73 <ashul2173@gmail.com>
2022-01-19 21:41:53 +0530

    repl automation and better responses

--- src/main/java/com/diablo73/springApps/SpringBootNoSQLApplication/Controllers/Controller.java
+++ src/main/java/com/diablo73/springApps/SpringBootNoSQLApplication/Controllers/Controller.java
@@ -5,12 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class Controller {
 
     @GetMapping("/")
     public ResponseEntity<String> defaultMessage() {
-        return new ResponseEntity<String>("Welcome to the SpringBootNoSQLApplication Started Successfully!!!", HttpStatus.OK);
+        Date date = new Date();
+        return new ResponseEntity<String>(
+                "Welcome to the SpringBootNoSQLApplication!!!" +
+                        "<br>" +
+                        "Started Successfully at Time : " + date,
+                HttpStatus.OK);
     }
 
 }
--- src/main/java/com/diablo73/springApps/SpringBootNoSQLApplication/SpringBootNoSQLApplication.java
+++ src/main/java/com/diablo73/springApps/SpringBootNoSQLApplication/SpringBootNoSQLApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootNoSQLApplication {
 
+	private static final String ENDPOINT = "https://springboot-nosql-application.diablo73.repl.co/";
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootNoSQLApplication.class, args);
+		System.out.println("\nEndpoint for SpringBootNoSQLApplication :\n" + ENDPOINT + "\n");
 	}
 
 }
